24c28b153e48993298a1728e9dc6e12cb39a1c1c
uziarslan <143803590+uziarslan@users.noreply.github.com>
2025-02-25 19:37:08 +0500

    Babel fix

--- src/Components/Flash.js
+++ src/Components/Flash.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Flash({ message }) {
   const [visible, setVisible] = useState(true);
